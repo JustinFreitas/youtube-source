9ee6a44cb6bf76f79f784f3b3715afe889ebe41c
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-17 22:52:23 +0200

    fix: cipher: close embed response before getTimestamp to avoid connection starvation (#245)
    In CipherManager#getPlayerScript, getTimestamp was invoked within
    the try-with-resources block of the embed CloseableHttpResponse.
    
    Keeping the response open while executing another request on the same
    HttpInterface can lead to connection pool starvation and socket timeouts.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- common/src/main/java/dev/lavalink/youtube/cipher/CipherManager.java
+++ common/src/main/java/dev/lavalink/youtube/cipher/CipherManager.java
@@ -39,16 +39,21 @@ public interface CipherManager {
 
     default CachedPlayerScript getPlayerScript(@NotNull HttpInterface httpInterface) {
         synchronized (this) {
+            String scriptUrl;
             try (ClassicHttpResponse response = httpInterface.execute(new HttpGet("https://www.youtube.com/embed/"))) {
                 HttpClientTools.assertSuccessWithContent(response, "fetch player script (embed)");
 
                 String responseText = dev.lavalink.youtube.http.HttpUtils.entityToString(response.getEntity(), StandardCharsets.UTF_8);
-                String scriptUrl = DataFormatTools.extractBetween(responseText, "\"jsUrl\":\"", "\"");
+                scriptUrl = DataFormatTools.extractBetween(responseText, "\"jsUrl\":\"", "\"");
 
                 if (scriptUrl == null) {
                     throw new ExceptionWithResponseBody("no jsUrl found", responseText);
                 }
+            } catch (IOException e) {
+                throw ExceptionTools.toRuntimeException(e);
+            }
 
+            try {
                 return new CachedPlayerScript(scriptUrl, getTimestamp(httpInterface, scriptUrl));
             } catch (IOException e) {
                 throw ExceptionTools.toRuntimeException(e);
